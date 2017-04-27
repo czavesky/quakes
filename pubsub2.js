7d8d47cfd6d953d87581eb13d206a99540f6b6eb
Fortune Ekeruo <legalmody@gmail.com>
2017-04-27 17:41:23 +0100

    Show visitors online

--- pubsub2.js
+++ pubsub2.js
@@ -1,3 +1,6 @@
+// import { Mongo } from 'meteor/mongo';
+const Visitors = new Mongo.Collection('visitors');
+
 if (Meteor.isClient) {
   const Quakes = new Mongo.Collection('quakes');
 
@@ -7,7 +10,7 @@ if (Meteor.isClient) {
     var searchHandle = Meteor.subscribe('quakesSearch');
     Session.set('searching', !searchHandle.ready());
   });
-  
+
   var user_location = function() {
     var location = geoip.get('location');
     if (location) {
@@ -32,14 +35,56 @@ if (Meteor.isClient) {
   });
 
   var latitude_percent = function(latitude) {
-    return ((latitude * -1) + 90) / 180 * 100;  
+    return ((latitude * -1) + 90) / 180 * 100;
   };
   var longitude_percent = function(longitude) {
     return (longitude + 180) / 360 * 100;
   };
-  
+
+  Template.visitors.onCreated(function () {
+    Meteor.subscribe('visitors');
+
+    // Get visitors saved in the db
+    const savedVisitors = Visitors.find().collection._docs._map;
+    console.log(savedVisitors)
+    // Fetch users location
+    $.getJSON("http://freegeoip.net/json/", function(location) {
+      const locationInfor = {
+        country_code: location.country_code,
+        country: location.country_name,
+        city: location.city,
+        ip: location.ip,
+        time_zone: location.time_zone,
+        latitude: location.latitude,
+        longitude: location.longitude,
+        createdAt: new Date()
+      }
+      setTimeout(() => {
+        const visitorsOn = Object.keys(savedVisitors);
+        console.log(visitorsOn)
+        visitorsOn.forEach((visitor) => {
+          const pageWidth = $("body").width();
+          const randomXPoint = Math.round(Math.random()*pageWidth);
+          var randomYPoint = $("body").height() - Math.round(Math.random() * 200) + 20;
+          $("body").append('<div id='+ visitor +' style="border: 1px solid red; border-bottom: 10px solid red; border-radius: 5px; background: white; height: 20px; width: 5px; position:fixed; top: '+randomYPoint+'px; left: '+randomXPoint+'px"></div>')
+          setInterval(() => {
+            randomYPoint -= 10;
+            // $(`#${visitor}`).css('top', randomYPoint);
+            $(`#${visitor}`).animate({'top':randomYPoint, 'height':$(`#${visitor}`).height() + 10}, 1000);
+            // $(`#${visitor}`).css('height', $(`#${visitor}`).height() + 10);
+          }, 1000)
+        })
+      }, 3000);
+
+      // Insert a visitor into the collection
+      // Meteor.call('visitors.insert', locationInfor);
+      Meteor.call('visitors.upsert', locationInfor);
+      // Meteor.call('visitors.remove');
+    });
+  });
+
   Template.quake.helpers({
-    
+
     // Format the time
     time: function() {
       return (new Date(this.time)).toLocaleTimeString();
@@ -79,15 +124,14 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  
   Meteor.publish('quakesSearch', function() {
     var self = this;
     try {
-      
+
       // Feed is from https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
       var response = HTTP.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson');
-      
-      console.log(response);
+
+      // console.log(response);
 
       _.each(response.data.features, function(feature) {
         if (feature.properties && feature.geometry && feature.geometry.coordinates) {
@@ -100,7 +144,7 @@ if (Meteor.isServer) {
             depth: feature.geometry.coordinates[1],
             usgs_id: feature.id
           };
-  
+
           self.added('quakes', Random.id(), doc);
         }
       });
@@ -111,5 +155,27 @@ if (Meteor.isServer) {
       console.log(error);
     }
   });
-  
+
+  Meteor.publish('visitors', function visitorsPublication() {
+    return Visitors.find();
+  });
+
+  Meteor.methods({
+    'visitors.insert' (locationInfor) {
+      Visitors.insert(locationInfor);
+    },
+
+    'visitors.upsert' (locationInfor) {
+      Visitors.upsert({
+        ip: locationInfor.ip
+      }, {
+        $set: locationInfor
+      });
+    },
+
+    'visitors.remove' () {
+      Visitors.remove({});
+    }
+  })
+
 }
